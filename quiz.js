7ce55eeb4f450a3387cb81aa3820c40ffe9bd459
Lustro Awuah <shineawuah@gmail.com>
2025-01-19 20:49:04 +0000

    update

--- quiz.js
+++ quiz.js
@@ -1,33 +1,18 @@
 function checkAnswer() {
-
     const correctAnswer = "4";
-
     
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    
+    const userAnswerElement = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = userAnswerElement ? userAnswerElement.value : null; 
 
     
     const feedbackElement = document.getElementById('feedback');
-
     if (userAnswer) {
-        
-        const userAnswerValue = userAnswer.value;
-
-        
-        if (userAnswerValue === correctAnswer) {
-            
-            feedbackElement.textContent = "Correct! Well done.";
-            feedbackElement.style.color = "green"; 
+        if (userAnswer === correctAnswer) {
+            feedbackElement.textContent = "Correct! Well done."; 
+            feedbackElement.style.color = "green";
         } else {
-            
-            feedbackElement.textContent = "That's incorrect. Try again!";
-            feedbackElement.style.color = "red"; 
-        }
-    } else {
-        
-        feedbackElement.textContent = "Please select an answer.";
-        feedbackElement.style.color = "orange"; 
-    }
-}
+            feedback
 
 
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
